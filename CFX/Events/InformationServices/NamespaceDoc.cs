1cc90bf7569d10fe8febce3f18722c0e86ad69e5
John Walls <jwalls@aiscorp.com>
2017-01-12 13:56:25 -0500

    Updated Documentation

--- CFX/Events/InformationServices/NamespaceDoc.cs
+++ CFX/Events/InformationServices/NamespaceDoc.cs
@@ -9,7 +9,6 @@ namespace CFX.Events.InformationServices
     /// The <see cref="CFX.Events.InformationServices"/> namespace contains messages related to interaction 
     /// with higher level level information systems running in the factory, such as ERP (Enterprise Resource Planning) 
     /// or MES (Manufacturing Execution Systems).  
-    /// This is a test.
     /// </summary>
     [System.Runtime.CompilerServices.CompilerGenerated]
     public class NamespaceDoc
