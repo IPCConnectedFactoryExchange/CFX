5aaad8f7d39131e0e11f6b74791485d0b6a5e9e0
John Walls <jwalls@aiscorp.com>
2018-01-10 11:00:17 -0500

    Documentation updates

---
+++ APEX CFX Demo App/APEX CFX Demo App/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace APEX_CFX_Demo_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(127, 33);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(218, 33);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(127, 115);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(166, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 169);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "APEX CFX Demo App";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ APEX CFX Demo App/APEX CFX Demo App/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CFX;
+using CFX.Structures;
+using CFX.ResourcePerformance;
+using CFX.Transport;
+
+namespace APEX_CFX_Demo_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lastStateChange = DateTime.Now;
+        }
+
+        AmqpCFXEndpoint theEndpoint;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (theEndpoint != null) btnClose_Click(sender, e);
+
+            theEndpoint = new AmqpCFXEndpoint();
+            theEndpoint.Open("MyHandle-" + Guid.NewGuid().ToString());
+            theEndpoint.AddPublishChannel(new Uri("amqp://cfx.aiscorp.com:5672"), "/exchange/AegisCloud");
+            btnSend.Enabled = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+            if (theEndpoint != null)
+            {
+                theEndpoint.Close();
+                theEndpoint = null;
+            }
+        }
+
+        private ResourceState currentState = ResourceState.Off;
+        private DateTime lastStateChange;
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            CFXEnvelope msg;
+
+            if (currentState == ResourceState.Off)
+            {
+                msg = new CFXEnvelope(new StationStateChanged()
+                {
+                    NewState = ResourceState.On,
+                    OldState = currentState,
+                    OldStateDuration = DateTime.Now - lastStateChange,
+                });
+            }
+            else
+            {
+                msg = new CFXEnvelope(new StationStateChanged()
+                {
+                    NewState = ResourceState.Off,
+                    OldState = currentState,
+                    OldStateDuration = DateTime.Now - lastStateChange,
+                });
+            }
+
+            lastStateChange = msg.TimeStamp;
+            currentState = (msg.MessageBody as StationStateChanged).NewState;
+
+            theEndpoint.Publish(msg);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnClose_Click(sender, e);
+        }
+    }
+}
