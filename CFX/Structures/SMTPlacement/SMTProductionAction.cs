54d0d73b66722963530aa68d891afd5c9681c056
alexisfouquet <alexis.fouquet@europlacer.fr>
2018-07-09 15:20:00 +0200

    Adding new messages for SMT production action performing

---
+++ CFX/Structures/SMTPlacement/SMTProductionAction.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// Possible action in production (applicable for a unit)
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum SMTProductionAction
+    {
+        /// <summary>
+        /// Manual operation on the unit
+        /// </summary>
+        ManualOperation,
+        /// <summary>
+        /// Board Conveying for a unit
+        /// </summary>
+        BoardConveying,
+        /// <summary>
+        /// Board alignment for a unit
+        /// </summary>
+        BoardAlignment,
+        /// <summary>
+        /// Placement on a unit
+        /// </summary>
+        Placement,
+    }
+}
