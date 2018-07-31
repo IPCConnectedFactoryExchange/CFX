87a750c8ef897b91c4ce25d41b5a53c07f0de6ca
John Walls <jwalls@aiscorp.com>
2018-07-31 10:56:34 -0400

    Added solective soldering messages on behalf of Pillarhouse.

---
+++ CFX/Structures/SolderDispensing/NamespaceDoc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFX.Structures.SolderDispensing
+{
+    /// <summary>
+    /// Data structures related to Solder Dispensing equipment
+    /// </summary>
+    internal static class NamespaceDoc
+    {
+    }
+}
