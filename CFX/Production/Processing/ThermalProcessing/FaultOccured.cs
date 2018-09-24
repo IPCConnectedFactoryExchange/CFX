4751484692e31fb9994a446d486255cb9ee752fd
rwilshusen-kic <41592397+rwilshusen-kic@users.noreply.github.com>
2018-09-24 15:17:04 -0700

    fixed formatting

--- CFX/Production/Processing/ThermalProcessing/FaultOccured.cs
+++ CFX/Production/Processing/ThermalProcessing/FaultOccured.cs
@@ -7,18 +7,18 @@ namespace CFX.Production.Processing.ThermalProcessing
     /// <summary>
     /// Sent by a process endpoint after a fault occurs.
     /// <code language="none">
-	/*
-  {
-    "$type": "CFX.Structures.SimulatedProfiling.SimulatedProfilingFault, CFX",
-    "TimeDateFaultEvent": "20080915T155312",
-    "Cause": “PROCESSOUTOFSPEC”,
-    "Severity": "WARNING",
-    "FaultCode": null,
-    "FaultOccurrenceId": null,
-    "Lane": null,
-    "Stage": null
-  }
-	*/
+    /*
+    {
+	"$type": "CFX.Structures.SimulatedProfiling.SimulatedProfilingFault, CFX",
+	"TimeDateFaultEvent": "20080915T155312",
+	"Cause": “PROCESSOUTOFSPEC”,
+	"Severity": "WARNING",
+	"FaultCode": null,
+	"FaultOccurrenceId": null,
+	"Lane": null,
+	"Stage": null
+    }
+    */
     /// </code>
     /// </summary>
     public class SimulatedProfilingFaultOccured : CFXMessage
