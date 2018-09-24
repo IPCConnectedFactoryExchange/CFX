545835b4c8b3b6054f3a2b85badf59c6b4896441
rwilshusen-kic <41592397+rwilshusen-kic@users.noreply.github.com>
2018-09-24 14:39:27 -0700

    initial commit of ThermalProcessing Namespace

---
+++ CFX/Production/Processing/ThermalProcessing/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Production.Processing.ThermalProcessing
+{
+    /// <summary>
+    /// CFX Topic implemented by endpoints which are involved in the processing of production units.
+    /// </summary>
+    internal static class NamespaceDoc
+    {
+    }
+}
