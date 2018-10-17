1bed17f189127808134a187a151130d59e3788f7
Ryan Wilshusen <rwilshusen@us.kic>
2018-10-17 16:45:54 -0700

    Adding reflow profiling structures.

---
+++ CFX/Structures/ReflowProfiling/FaultClear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Structures.ReflowProfiling
+{
+    /// <summary>
+    /// A dynamic structure which describes a fault that has occurred in the course of production.
+    /// </summary>
+    public class FaultClear : Fault
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public FaultClear()
+        {
+        }
+
+        /// <summary>
+        /// Time and date fault was cleared in ISO 8061 Datetime format. 
+        /// </summary>
+        public string TimeDateFaultEvent
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CFX/Structures/ReflowProfiling/LogEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Structures.ReflowProfiling
+{
+    /// <summary>
+    /// A dynamic structure which describes a log event that has occurred in the course of production.
+    /// </summary>
+    public class LogEntry : Fault
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public LogEntry()
+        {
+        }
+
+        /// <summary>
+        /// Time and date of log occurence in ISO 8061 Datetime format. 
+        /// </summary>
+        public string TimeDateFaultEvent
+        {
+            get;
+            set;
+        }
+    }
+}
