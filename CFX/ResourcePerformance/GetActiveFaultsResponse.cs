907bcd7e9ebfbf044d9f2ca82881d093b7a24482
Cord Burmeister <Cord.Burmeister@asmpt.com>
2018-10-30 11:32:33 +0100

    Improve assist handling with more details

---
+++ CFX/ResourcePerformance/GetActiveFaultsResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CFX.Structures;
+
+namespace CFX.ResourcePerformance
+{
+    /// <summary>
+    /// Response to an external source to modify a generic configuration parameter of a process endpoint.
+    /// </summary>
+    public class GetActiveFaultsResponse : CFXMessage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetActiveFaultsResponse"/> class.
+        /// </summary>
+        public GetActiveFaultsResponse()
+        {
+            Result = new RequestResult();
+            ActiveFaults = new List<Fault>();
+        }
+
+        /// <summary>
+        /// The result of the request
+        /// </summary>
+        public RequestResult Result
+        {
+            get;
+            set;
+        }
+
+
+        public List<Fault> ActiveFaults
+        {
+            get;
+            set;
+        }
+    }
+}
