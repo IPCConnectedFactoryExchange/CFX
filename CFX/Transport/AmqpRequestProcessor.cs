ca816e71e8e5a0bfa86b865965614ebb6daee0ee
John Walls <jwalls@aiscorp.com>
2018-12-19 09:54:44 -0500

    Updated VS 2015 solution

--- CFX/Transport/AmqpRequestProcessor.cs
+++ CFX/Transport/AmqpRequestProcessor.cs
@@ -141,7 +141,8 @@ namespace CFX.Transport
             string t = targetAddress.ToUpper();
             if (!listeners.ContainsKey(t)) throw new Exception("The specified targetAddress does not have an active listener.");
             inboundHost.UnregisterMessageProcessor(targetAddress);
-            while (!listeners.TryRemove(targetAddress, out InternalMessageProcessor p)) Task.Yield();
+            InternalMessageProcessor p = null;
+            while (!listeners.TryRemove(targetAddress, out p)) Task.Yield();
         }
 
         public void Close()
