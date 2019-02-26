532a09041d0f73e2cefacd45802001908ecea671
Lars Rosenboom <lars.rosenboom@viscom.de>
2019-02-26 18:57:43 +0100

    Added some comments.

--- CFX/Production/TestAndInspection/RecipeChanged.cs
+++ CFX/Production/TestAndInspection/RecipeChanged.cs
@@ -7,7 +7,7 @@ namespace CFX.Production.TestAndInspection
 
   /// <summary>
   ///   A recipe describes, starting with the panel as root object, all components to inspect.
-  ///   It does not (yet) contain any inspection results.
+  ///   It does not contain any inspection results. Those are transmitted via the DefectsDetected message.
   ///   
   ///   The name is inspired from SECS/GEM "recipe". Synonyms are "inspection program" (in the domain / field of AOI/AXI)
   ///   or "data model" (in Viscom vVision).
