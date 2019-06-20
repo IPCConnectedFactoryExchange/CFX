353933e635a69a5706208242be9622dec176e324
John Walls <johnjwallsiii@gmail.com>
2019-06-20 15:49:38 -0400

    Exposing Newtonsoft JSON serialization settings as static variable to that SDK users can more finely control formatting, verbosity, and other settings when converting objects to and from JSON.

--- CFX/Utilities/JsonConvert.cs
+++ CFX/Utilities/JsonConvert.cs
@@ -12,33 +12,39 @@ namespace CFX.Utilities
         public static string SerializeObject(object o, bool formatted = false)
         {
             Newtonsoft.Json.Formatting format = formatted ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None;
-            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
-            settings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None;
-            settings.TypeNameAssemblyFormatHandling = Newtonsoft.Json.TypeNameAssemblyFormatHandling.Simple;
-            return Newtonsoft.Json.JsonConvert.SerializeObject(o, format, settings);
+            if (JsonSettings == null) JsonSettings = new Newtonsoft.Json.JsonSerializerSettings();
+            JsonSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None;
+            JsonSettings.TypeNameAssemblyFormatHandling = Newtonsoft.Json.TypeNameAssemblyFormatHandling.Simple;
+            return Newtonsoft.Json.JsonConvert.SerializeObject(o, format, JsonSettings);
         }
 
         public static string SerializeObjectWithTypes(object o, bool formatted = false)
         {
             Newtonsoft.Json.Formatting format = formatted ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None;
-            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
-            settings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            settings.TypeNameAssemblyFormatHandling = Newtonsoft.Json.TypeNameAssemblyFormatHandling.Simple;
-            return Newtonsoft.Json.JsonConvert.SerializeObject(o, format, settings);
+            if (JsonSettings == null) JsonSettings = new Newtonsoft.Json.JsonSerializerSettings();
+            JsonSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+            JsonSettings.TypeNameAssemblyFormatHandling = Newtonsoft.Json.TypeNameAssemblyFormatHandling.Simple;
+            return Newtonsoft.Json.JsonConvert.SerializeObject(o, format, JsonSettings);
         }
 
         public static T DeserializeObject<T>(string jsonData)
         {
-            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
-            settings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonData, settings);
+            if (JsonSettings == null) JsonSettings = new Newtonsoft.Json.JsonSerializerSettings();
+            JsonSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonData, JsonSettings);
         }
 
         public static object DeserializeObject(string jsonData, Type type)
         {
-            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
-            settings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            return Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData, type, settings);
+            if (JsonSettings == null) JsonSettings = new Newtonsoft.Json.JsonSerializerSettings();
+            JsonSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+            return Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData, type, JsonSettings);
+        }
+
+        public static Newtonsoft.Json.JsonSerializerSettings JsonSettings
+        {
+            get;
+            set;
         }
     }
 }
