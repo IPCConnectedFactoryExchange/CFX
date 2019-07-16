45c4ec44d0f88da8e43929a42bb935e420191c86
Cord Burmeister <Cord.Burmeister@asmpt.com>
2019-07-16 12:46:04 +0200

    Add changes from Integration

--- CFX/Structures/PCBInspection/Board.cs
+++ CFX/Structures/PCBInspection/Board.cs
@@ -43,6 +43,9 @@ namespace CFX.Structures.PCBInspection
     {
       get
       {
+          if (IsRepaired)
+              return false;  // The panel as a whole was repaired, so it is not defect anymore.
+
         if (base.IsDefect)
           return true;  // The inspection object itself is defect.
 
