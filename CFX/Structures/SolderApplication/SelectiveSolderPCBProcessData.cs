6b88e982c9b385dd289c960f3c1a84b3666db179
John Walls <johnjwallsiii@gmail.com>
2019-08-08 16:00:34 -0400

    Refactoring Selective Solder messaging to CFX.Processing.UnitsProcessed dynamic structure.  PCBSelectivelySoldered message depreciated for CFX 1.1

---
+++ CFX/Structures/SolderApplication/SelectiveSolderPCBProcessData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFX.Structures.SolderApplication
+{
+    /// <summary>
+    /// Process Data specific to a single PCB within a group of PCB's that are selectively
+    /// soldered.
+    /// </summary>
+    public class SelectiveSolderPCBProcessData : ProcessData
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SelectiveSolderPCBProcessData()
+        {
+            ZoneData = new List<ZoneData>();
+        }
+
+        /// <summary>
+        /// Data sepcific to a single zone with the
+        /// Selective Soldering System
+        /// </summary>
+        public List<ZoneData> ZoneData
+        {
+            get;
+            set;
+        }
+    }
+}
