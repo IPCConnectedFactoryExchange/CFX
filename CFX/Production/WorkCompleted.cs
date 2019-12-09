5f7631d534725be556c52e1a5a5b661d82930016
John Walls <johnjwallsiii@gmail.com>
2019-12-09 12:03:25 -0500

    Updating UnitsInitialized and WorkCompleted messages per committee meeting on December 9th.

--- CFX/Production/WorkCompleted.cs
+++ CFX/Production/WorkCompleted.cs
@@ -22,6 +22,14 @@ namespace CFX.Production
     public class WorkCompleted : CFXMessage 
     {
         /// <summary>
+        /// Default constructor
+        /// </summary>
+        WorkCompleted()
+        {
+            Units = new List<UnitPosition>();
+        }
+
+        /// <summary>
         /// Related Transaction ID specified previously by WorkStarted Message
         /// </summary>
         public Guid TransactionID
@@ -30,7 +38,71 @@ namespace CFX.Production
             set;
         }
 
+        /// <summary>
+        /// An enumeration indicating the overall result of the work transaction.
+        /// </summary>
         public WorkResult Result
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// OPTIONAL.  Only required if the actual identifiers were not available at the time that the trasnaction was started (at the time 
+        /// the WorkStarted message was transmitted).
+        /// The barcode, RFID, etc. that was most recently acquired by the scanner / reader.  If a single production unit is moving through the
+        /// process, this would be the actual unique identifier of that individual unition unit.  However, if multiple production units are moving
+        /// through the process as a group (as in the case of a PCB panel, a fixture, or any sort of carrier), this would be an identifier that
+        /// represents the entire group of units, such as a carrier UID, a PCB panel UID, etc.
+        /// </summary>
+        public string PrimaryIdentifier
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// OPTIONAL.  Only required if the actual identifiers were not available at the time that the trasnaction was started (at the time 
+        /// the WorkStarted message was transmitted).
+        /// The Hermes BoardId of the carrier, a PCB panel or single production unit. If a single production unit is moving through the
+        /// process, this would be the actual unique identifier of that individual unition unit.  However, if multiple production units are moving
+        /// through the process as a group (as in the case of a PCB panel, a fixture, or any sort of carrier), this would be an identifier that
+        /// represents the entire group of units, such as a carrier UID, a PCB panel UID, etc.
+        /// HermesIdentifier will be transfered between all machines which support Hermes. The PrimaryIdentifier is containing a barcode information.
+        /// Both can be transferred.
+        /// <remarks>
+        /// Espcially when the line does not support the Hermes standard in the hole line, the Hermes Identifier can be unique only in the a part
+        /// of the line. The Primary Identifier can be used to correlate the parts of hermes sublines to correlate this data as well. 
+        /// </remarks>
+        /// </summary>
+        public string HermesIdentifier
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// OPTIONAL.  Only required if the actual identifiers were not available at the time that the trasnaction was started (at the time 
+        /// the WorkStarted message was transmitted).
+        /// The number of individual production units associated with this transaction.  May be more than 1 in the case of a carrier or panel of multiple PCB’s.
+        /// </summary>
+        public int UnitCount
+        {
+            get
+            {
+                return Units.Count;
+            }
+            private set
+            {
+            }
+        }
+
+        /// <summary>
+        /// OPTIONAL.  Only required if the actual identifiers were not available at the time that the trasnaction was started (at the time 
+        /// the WorkStarted message was transmitted).
+        /// Data that identifies each specific instance of production unit with a carrier or panel. 
+        /// </summary>
+        public List<UnitPosition> Units
         {
             get;
             set;
