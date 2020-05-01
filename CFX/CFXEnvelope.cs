b63f4cb323a8aa8fb45c31d85c9feb657ba22c43
John Walls <johnjwallsiii@gmail.com>
2020-05-01 09:48:30 -0400

    Fixed Version property of CFXEnvelope.  Was accidentally set to 1.2 in version via a faulty code merge.  Resetting back to 1.1.  Also, updated version to 1.1.13

--- CFX/CFXEnvelope.cs
+++ CFX/CFXEnvelope.cs
@@ -45,7 +45,7 @@ namespace CFX
             Transmitted = false;
         }
 
-        public const string CFXVERSION = "1.2";
+        public const string CFXVERSION = "1.1";
         
         public CFXEnvelope(Type messageType) : this()
         {
