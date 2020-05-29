ba09b839f8e6f9e8a20dc30dd7986cb4497c35fa
Alexis FOUQUET <40395554+alexisfouqueteuroplacer@users.noreply.github.com>
2020-05-29 17:20:10 +0100

    Addition of the WorkOrderActionExecuted message (#82)
    * Fixing bug created by changes to WorkCompleted message.
    
    * Updating to latest AMQPNetLite and Newtonsoft Nuget packages.
    
    * Addition of the WorkOrderActionExecuted message
    
    * Some cleanup for new WorkOrderActionExecuted message and related structures.
    
    Co-authored-by: John Walls <johnjwallsiii@gmail.com>
    Co-authored-by: Alexis FOUQUET <afouquet@epi.local>

---
+++ CFX/Structures/WorkOrderActionState.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// Describes the state of a work order action.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    [CFX.Utilities.CreatedVersion("1.2")]
+    public enum WorkOrderActionState
+    {
+        /// <summary>
+        /// The work order action has started, but is not yet complete.
+        /// </summary>
+        Started,
+        /// <summary>
+        /// The work order action was aborted.
+        /// </summary>
+        Aborted,
+        /// <summary>
+        /// The work order action successfullly completed.
+        /// </summary>
+        Completed
+    }
+}
