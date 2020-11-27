f6883b7a4be4ae74c6d43e9b14c5dee324b8de09
Lars Rosenboom <lars.rosenboom@viiscom.de>
2020-11-27 18:04:42 +0100

    Do _not_ set CFXEnvelope.Version to the version of the CFX-DLL in MessageBody.set, as that, when receiving a CFX message, overwrites the actual version of the sender (with CFXVERSION of the receiving CFX-DLL). When sending a CFX message the Version is initialized correctly (to CFXVERSION of the sending CFX-DLL) in the constructor of CFXEnvelope anyway.

--- CFX/CFXEnvelope.cs
+++ CFX/CFXEnvelope.cs
@@ -169,7 +169,6 @@ namespace CFX
                 if (messageBody != null && MessageBody.GetType().FullName.StartsWith("CFX."))
                 {
                     MessageName = messageBody.GetType().FullName;
-                    Version = CFXVERSION;
                 }
             }
         }
