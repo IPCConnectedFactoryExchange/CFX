4a6185ca417e111c1f7ab68726469c67adfb6adf
Alexis FOUQUET <alexis.fouquet@europlacer.fr>
2021-04-20 14:28:32 +0200

    Revert "On a machine, it appears that a vision test can be done before the pickup (to check that this is the good component, with good orientation). This option was missing in the enum."
    This reverts commit 4870c2838dc59550066b6c868af0376565078ff8.

--- CFX/Structures/RejectionReason.cs
+++ CFX/Structures/RejectionReason.cs
@@ -69,10 +69,6 @@ namespace CFX.Structures
         /// <summary>
         /// The component was rejected because of a bad optical test after placement
         /// </summary>
-        BadOpticalTestAfterPlacement,
-        /// <summary>
-        /// The component was rejected because of a bad vision test before pickup
-        /// </summary>
-        BadVisionTestBeforePickup,
+        BadOpticalTestAfterPlacement
     }
 }
