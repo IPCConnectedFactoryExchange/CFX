dd29720fbf460983b3c071749d27f861ac73ea9e
Daniele Isola <daniele.isola@asm-smt.com>
2022-03-30 11:30:29 +0200

    Tools cleaning management

--- CFX/Structures/SolderPastePrinting/SMTSqueegee.cs
+++ CFX/Structures/SolderPastePrinting/SMTSqueegee.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using CFX.Structures.Cleaning;
 
 namespace CFX.Structures.SolderPastePrinting
 {
@@ -9,5 +12,15 @@ namespace CFX.Structures.SolderPastePrinting
     /// </summary>
     public class SMTSqueegee : Tool
     {
+        /// <summary>
+        /// SMT Squeegee cleaning states
+        /// </summary>
+        [CFX.Utilities.CreatedVersion("1.5")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CleaningState CleaningState
+        {
+            get;
+            set;
+        }
     }
 }
