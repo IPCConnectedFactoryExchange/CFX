6e2888dc786f59c8f0b06fd851d7d16fd0184d51
John Walls <jwalls@aiscorp.com>
2022-04-07 08:23:03 -0400

    Jjw merge router (#160)
    * RouterToolParameters Added as per Team2-17-AT
    
    * Fixing up depanel / router submission due to merge issues
    
    Co-authored-by: sarumathi@getecha.com <71495114+SarumathiGetech@users.noreply.github.com>
    Co-authored-by: John Walls <johnjwallsiii@gmail.com>

---
+++ CFX/Structures/Depaneling/Axis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Structures.Depaneling
+{
+    /// <summary>
+    /// Represents an axis on a depaneling / router type machine.
+    /// <para>** NOTE: ADDED in CFX 1.5 **</para>
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("1.5")]
+    public class Axis
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Axis()
+        {
+
+        }
+
+        /// <summary>
+        /// Name of the Axis 
+        /// </summary>
+        public string AxisName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Max axis speed in mm/s
+        /// </summary>
+        public decimal AxisSpeed
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Set value in (mm/s2)
+        /// /// <summary>
+        public decimal AxisAcceleration
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Set value in (mm/s2)
+        /// </summary>
+        public decimal AxisDeceleration
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Operating at Maximum speed.
+        /// </summary>
+        public decimal ActualPeakVelocity
+        {
+            get;
+            set;
+        }
+    }
+}
