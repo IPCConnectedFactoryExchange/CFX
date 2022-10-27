27de48c21f24ad8b850b57a1d986258921eaa1dd
Alexis FOUQUET <40395554+alexisfouqueteuroplacer@users.noreply.github.com>
2022-10-27 13:56:46 +0200

    Adding Windows 11 to the operating systems data structure (#165)
    * Fixing bug created by changes to WorkCompleted message.
    
    * Updating to latest AMQPNetLite and Newtonsoft Nuget packages.
    
    * Addition of a comment to specify that the format of the timestamp must include the time zone, whether expressed in local time or UTC.
    
    * Revert "Addition of a comment to specify that the format of the timestamp must include the time zone, whether expressed in local time or UTC."
    
    This reverts commit adcaf3d305fb05344bc0b335c6fa71d069ea98f5.
    
    * Addition of the bulk case feeder
    
    * Adding Windows 11 to the operating systems data structure
    
    Co-authored-by: John Walls <johnjwallsiii@gmail.com>

--- CFX/Structures/Endpoint.cs
+++ CFX/Structures/Endpoint.cs
@@ -339,7 +339,13 @@ namespace CFX.Structures
         /// <summary>
         /// Raspberry Pi Operating System
         /// </summary>
-        Raspbian
+        Raspbian,
+        /// <summary>
+        /// Windows 11
+        /// <para>** NOTE: ADDED in CFX 1.5 **</para>
+        /// </summary>
+        [CFX.Utilities.CreatedVersion("1.5")]
+        Windows11
     }
 
     /// <summary>
