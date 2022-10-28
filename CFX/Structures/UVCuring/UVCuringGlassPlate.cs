2b2f46a5486aab7d2d3481a38911043ef214f12f
Andreas Hanslovsky <116063959+AHanslovsky@users.noreply.github.com>
2022-10-28 17:55:25 +0200

    Added ProcessData Structure for UV Curing (#177)
    * Additional UV Curring structures added
    
    * Fixed spelling mistakes and documentation
    
    * Update UVCuringGlassPlate.cs
    
    Adding Version Identification attributes
    
    * Update UVCuringLampData.cs
    
    Adding version identification attributes
    
    * Update UVCuringLampState.cs
    
    Adding version identification attributes
    
    * Update UVCuringProcessData.cs
    
    Adding version identification attributes.
    
    Co-authored-by: John Walls <jwalls@aiscorp.com>

---
+++ CFX/Structures/UVCuring/UVCuringGlassPlate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Structures.UVCuring
+{
+    /// <summary>
+    /// Properties of glass plate.
+    /// <para>** NOTE: ADDED in CFX 1.6 **</para>
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("1.6")]
+    public class UVCuringGlassPlate
+    {
+        /// <summary>
+        /// Row of the glas plate
+        /// </summary>
+        public int Row { get; set; }
+
+
+        /// <summary>
+        /// Is the glas plate present
+        /// </summary>
+        public bool IsPresent { get; set; }
+    }
+}
