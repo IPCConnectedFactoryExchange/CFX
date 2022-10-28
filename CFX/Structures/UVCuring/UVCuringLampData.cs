3c3c4b883d8df77e5da71eaa52be69c541f83001
John Walls <jwalls@aiscorp.com>
2022-10-28 11:33:09 -0400

    Update UVCuringLampData.cs
    Adding version identification attributes

--- CFX/Structures/UVCuring/UVCuringLampData.cs
+++ CFX/Structures/UVCuring/UVCuringLampData.cs
@@ -6,7 +6,9 @@ namespace CFX.Structures.UVCuring
 {
     /// <summary>
     /// Properties of an UV lamp. 
+    /// <para>** NOTE: ADDED in CFX 1.6 **</para>
     /// </summary>
+    [CFX.Utilities.CreatedVersion("1.6")]
     public class UVCuringLampData
     {
         /// <summary>
