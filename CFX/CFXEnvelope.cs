fe1158e062fc78beeaab33bf9e1bc3818661d1a0
Alexis FOUQUET <alexis.fouquet@europlacer.fr>
2022-12-05 08:39:28 +0100

    Updating the CFX version in the messages envelope

--- CFX/CFXEnvelope.cs
+++ CFX/CFXEnvelope.cs
@@ -45,7 +45,7 @@ namespace CFX
             Transmitted = false;
         }
 
-        public const string CFXVERSION = "1.4";
+        public const string CFXVERSION = "1.6";
         
         public CFXEnvelope(Type messageType) : this()
         {
