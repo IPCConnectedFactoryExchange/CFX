174068552a5ef2a905165f2789be4028ee529cd3
Tim Burke <tim@archsys.io>
2023-05-02 10:24:25 -0700

    Feat heartbeats and operators (#189)
    * Edit namespace from PR #169 for merging.
    
    * Add active faults and active reipes into Heartbeat message.
    
    * Add decorators and comments to reference CFX 1.7 version additions.
    
    * Update Heartbeat.cs
    
    ---------
    
    Co-authored-by: John Walls <jwalls@aiscorp.com>

---
+++ CFX/InformationSystem/ValidateOperatorRequest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CFX.Structures;
+
+namespace CFX.InformationSystem
+{
+    /// <summary>
+    /// Request that an operator take action now or be responsible for a process endpoint.
+    /// The opposite endpoint can accept or reject this. Can be used if the MES has 
+    /// advanced user management. Multiple operators must be requested separately. 
+    /// <code language="none">
+    /// {
+    ///   "Operator": {
+    ///     "OperatorIdentifier": "42b7a5cc-3bbd-4010-8a01-1c5851b9a2a3",
+    ///     "ActorType": "Human",
+    ///     "LastName": "Smith",
+    ///     "FirstName": "Bill",
+    ///     "LoginName": "bill.smith@domain1.com"
+    ///   }
+    /// }
+    /// </code>
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("1.7")]
+    public class ValidateOperatorRequest : CFXMessage
+    {
+        /// <summary>
+        /// A structure which defines the Operator.
+        /// </summary>
+        public Operator Operator
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CFX/InformationSystem/ValidateOperatorResponse.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CFX.Structures;
+
+namespace CFX.InformationSystem
+{
+    /// <summary>
+    /// Response to a request that the operator's login was successful or not.
+    /// <code language="none">
+    /// {
+    ///   "Result": {
+    ///     "Result": "Success",
+    ///     "ResultCode": 0,
+    ///     "Message": "OK"
+    ///   }
+    /// }
+    /// </code>
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("1.7")]
+    public class ValidateOperatorResponse : CFXMessage
+    {
+        /// <summary>
+        /// The result of the request
+        /// </summary>
+        public RequestResult Result
+        {
+            get;
+            set;
+        }
+    }
+}
