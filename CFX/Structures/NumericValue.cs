03720d37a7955fa2b69b5bde1fd8ad0fea85acd3
DanieleASM <daniele.isola@asmpt.com>
2023-10-12 08:55:41 +0200

    add Units to NumericValue and optional Z to UnitPosition

--- CFX/Structures/NumericValue.cs
+++ CFX/Structures/NumericValue.cs
@@ -53,11 +53,30 @@ namespace CFX.Structures
             get;
             set;
         }
-
+        /// <summary>
+        /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+        /// The units of the minimum acceptable value (must be a valid SI unit)
+        /// </summary>
+        [CFX.Utilities.CreatedVersion("2.0")]
+        public string MinimumAcceptableValueUnits
+        {
+            get;
+            set;
+        }
         /// <summary>
         /// The minimum acceptable value
         /// </summary>
         public double? MaximumAcceptableValue
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+        /// The units of the maximum acceptable value (must be a valid SI unit)
+        /// </summary>
+        [CFX.Utilities.CreatedVersion("2.0")]
+        public string MaximumAcceptableValueUnits
         {
             get;
             set;
