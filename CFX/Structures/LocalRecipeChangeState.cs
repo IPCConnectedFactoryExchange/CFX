3eb816f911d17044a6967a12c766e2d716cd1634
Alexis FOUQUET <alexis.fouquet@europlacer.fr>
2024-02-12 16:45:32 +0100

    Creation of the LocalRecipeChanged message and the associated structures and enumerations

---
+++ CFX/Structures/LocalRecipeChangeState.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// State of a local recipe change
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("2.0")]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum LocalRecipeChangeState
+    {
+        /// <summary>
+        /// The change is effective.
+        /// </summary>
+        Activated,
+        /// <summary>
+        /// The change is removed back.
+        /// </summary>
+        Deactivated
+    }
+}
---
+++ CFX/Structures/LocalRecipeChangeType.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// Type of a local recipe change
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("2.0")]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum LocalRecipeChangeType
+    {
+        /// <summary>
+        /// Unit skip or unskip (UnitSkipProductionChange).
+        /// </summary>
+        UnitSkip,
+        /// <summary>
+        /// Component removal or restoration (ComponentRemovalProductionChange).
+        /// </summary>
+        ComponentRemoval,
+        /// <summary>
+        /// Use or not an alternative component (ComponentAlternativeProductionChange).
+        /// </summary>
+        ComponentAlternative,
+        /// <summary>
+        /// New teaching of a fiducial
+        /// </summary>
+        FiducialTeach
+    }
+}
