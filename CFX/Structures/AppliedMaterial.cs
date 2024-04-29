bbde9e028fb801e7f1d53012c65298cf8673ba4e
DanieleASM <daniele.isola@asmpt.com>
2024-04-29 13:47:52 +0200

    Extension agreed by A-Team and implemented by ASMPT

---
+++ CFX/Structures/AppliedMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// Describes a single lot of material that was applied on a production unit, possibly
+    /// in specific locations on the production unit.
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("2.0")]
+    public class AppliedMaterial : InstalledMaterial
+    {
+        /// <summary>
+        /// The quantity of the material that was applied, with defined units and thresholds
+        /// </summary>
+        [JsonProperty(Order = 1)]
+        public NumericValue QuantityApplied
+        {
+            get;
+            set;
+        }
+
+    }
+}
