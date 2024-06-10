698550fb0ae5b48bf21844038f7149b9a6c7ed08
Maximilian Seyfried <Maximilian.Seyfried@kurtzersa.de>
2024-06-10 15:00:17 +0200

    added ConvectionSpeedValueUnit enum

---
+++ CFX/Structures/SolderWave/ConvectionSpeedValueUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFX.Structures.SolderWave
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// An enumeration of convection speed value units. 
+    /// </summary>
+    [CFX.Utilities.CreatedVersion("2.0")]
+    public enum ConvectionSpeedValueUnit
+    {
+        Percentage,
+        RPM,
+    }
+}
