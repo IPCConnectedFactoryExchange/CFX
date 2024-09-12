ddfcc2dcb073d9251f91f3ae9041f05857878aab
John Walls <jwalls@aiscorp.com>
2024-09-12 05:40:08 -0400

    Commenting out schema and example generation

--- CFXExampleEndpoint/MainForm.cs
+++ CFXExampleEndpoint/MainForm.cs
@@ -40,13 +40,13 @@ namespace CFXExampleEndpoint
             reqUsername.Text = Properties.Settings.Default.RequestUsername;
             reqPassword.Text = Properties.Settings.Default.RequestPassword;
 
-            SchemaGenerator schemaGenerator = new SchemaGenerator();
-            schemaGenerator.GenerateSchemas(@"e:\Stuff\CFXSchemas\");
+            //SchemaGenerator schemaGenerator = new SchemaGenerator();
+            //schemaGenerator.GenerateSchemas(@"e:\Stuff\CFXSchemas\");
 
-            CFXExampleGenerator gen = new CFXExampleGenerator();
-            string result = gen.GenerateAll();
-            System.IO.File.WriteAllText(CFX_JSON_Examples, result, Encoding.UTF8);
-            return;
+            //CFXExampleGenerator gen = new CFXExampleGenerator();
+            //string result = gen.GenerateAll();
+            //System.IO.File.WriteAllText(CFX_JSON_Examples, result, Encoding.UTF8);
+            //return;
 
             if (!string.IsNullOrWhiteSpace(CFXHandle))
                 OpenEndpoint();
