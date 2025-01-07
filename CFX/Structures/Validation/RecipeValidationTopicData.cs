198d3dc53337d7bbb1b5459ee69d4a4c7f4baa95
DanieleASM <daniele.isola@asmpt.com>
2025-01-07 14:50:01 +0100

    ASMPT Interlocking Topics 2.0 (#208)
    * Ref to NetFramework 4.8 to compile in VS2022
    
    * Interlocking Topics as specified by the A-Team.
    
    * Fixed overwrite of ValidateOperatorRequest
    
    * fix
    
    * Fixing version tags on project file.
    
    * Fixing Versions
    
    ---------
    
    Co-authored-by: John Walls <jwalls@aiscorp.com>

---
+++ CFX/Structures/Validation/RecipeValidationTopicData.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFX.Structures.Validation
+{
+    /// <summary>
+    /// <para> ** NOTE: ADDED in CFX 2.0 **</para>
+    /// Represents the information of a recipe that needs to be validated 
+    /// by <see cref="InformationSystem.TopicValidation.ValidateTopicRequest"/>.
+    /// </summary>
+    [JsonObject(ItemTypeNameHandling = TypeNameHandling.Auto)]
+    [CFX.Utilities.CreatedVersion("2.0")]
+    public class RecipeValidationTopicData : ValidationData
+    {
+        /// <summary>
+        /// The name of the recipe (may include full path, if applicable)
+        /// </summary>
+        public string RecipeName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An optional version number, e.g. “2.0”
+        /// </summary>
+        public string Revision
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// The optional number of the production lane where 
+        /// the recipe should be activated
+        /// </summary>
+        public int? Lane
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// A structure describing basic information about the stage.
+        /// </summary>
+        public Stage Stage
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CFX/Structures/ValidationTopic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// Types of validation topics
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    [CFX.Utilities.CreatedVersion("2.0")]
+    public enum ValidationTopic
+    {
+        /// <summary>
+        /// A validation that ensures proper tools are used
+        /// </summary>
+        Tools,
+        /// <summary>
+        /// A validation that ensures proper materials are used
+        /// </summary>
+        Materials,
+        /// <summary>
+        /// A validation that ensures proper recipe is used
+        /// </summary>
+        Recipe,
+        /// <summary>
+        /// A validation that ensures proper MaterialCarrier is used
+        /// </summary>
+        MaterialCarrier
+    }
+}
