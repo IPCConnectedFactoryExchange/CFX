ebdb040dcf3cb244b801934fba9b10ad7079539c
robert hanyecz <r.hanyecz@rehm-group.com>
2025-02-03 09:27:07 +0100

    removed newtonsoft from ProcessStepType

--- CFX/Structures/VaporPhaseSoldering/VaporPhaseSolderingProcessStepType.cs
+++ CFX/Structures/VaporPhaseSoldering/VaporPhaseSolderingProcessStepType.cs
@@ -1,16 +1,10 @@
-﻿using Newtonsoft.Json.Converters;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
-
-namespace CFX.Structures.VaporPhaseSoldering
+﻿namespace CFX.Structures.VaporPhaseSoldering
 {
+
     /// <summary>
     /// An enumeration indicating which type of process was executed in a vapor phase soldering machine step.
     /// <para>** NOTE: ADDED in CFX 2.0 **</para>
     /// </summary>
-    [JsonConverter(typeof(StringEnumConverter))]
     [CFX.Utilities.CreatedVersion("2.0")]
     public enum VaporPhaseSolderingProcessStepType
     {
