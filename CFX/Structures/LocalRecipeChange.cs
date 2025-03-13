dad4b60665611c31411c8d66ba6458363b103b0c
Alexis FOUQUET <40395554+alexisfouqueteuroplacer@users.noreply.github.com>
2025-03-13 16:27:19 +0100

    LocalRecipeChanged new message (#210)
    * Creation of the LocalRecipeChanged message and the associated structures and enumerations
    
    * Forgotten type of local recipe change

---
+++ CFX/Structures/LocalRecipeChange.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace CFX.Structures
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// Describe a local recipe change
+    /// This is a dynamic structure.
+    /// </summary>
+    [Utilities.CreatedVersion("2.0")]
+    [JsonObject(ItemTypeNameHandling = TypeNameHandling.Auto)]
+    public class LocalRecipeChange
+    {
+        /// </summary>
+        /// The type of local recipe change.
+        /// </summary>
+        public LocalRecipeChangeType Type
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The state of the local recipe change.
+        /// </summary>
+        public LocalRecipeChangeState State
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CFX/Structures/UnitSkipLocalRecipeChange.cs
@@ -0,0 +1,19 @@
+﻿namespace CFX.Structures.SMTPlacement
+{
+    /// <summary>
+    /// <para>** NOTE: ADDED in CFX 2.0 **</para>
+    /// Additional data for a unit skip (local recipe change)
+    /// </summary>
+    [Utilities.CreatedVersion("2.0")]
+    public class UnitSkipLocalRecipeChange : LocalRecipeChange
+    {
+        /// <summary>
+        /// Logical reference of production unit as defined by CFX position rule (see CFX standard section 5.6) that is removed or restored for production.
+        /// </summary>
+        public int PositionNumber
+        {
+            get;
+            set;
+        }
+    }
+}
